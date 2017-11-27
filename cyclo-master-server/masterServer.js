cb87ea94b2fe3b5ef4b49ffa0d9d123239e4571d
Jerico Alcaras <alcarasj@tcd.ie>
2017-11-27 00:35:36 +0000

    Improved console logging and slave now extracts zip.

--- cyclo-master-server/masterServer.js
+++ cyclo-master-server/masterServer.js
@@ -9,14 +9,17 @@ const request = require('request');
 const FormData = require('form-data');
 const AdmZip = require('adm-zip');
 const md5File = require("md5-file");
+const ping = require('ping');
+require('console-stamp')(console, { pattern: 'dd/mm/yyyy HH:MM:ss' });
 
 const PORT = 8080;
 const SLAVES = ['127.0.0.1:8081', '127.0.0.1:8082', '127.0.0.1:8083'];
 const URL = '/analyse';
 const ZIPDIR = './slaveZips';
+const TMPDIR = './tmp';
 
-if (!fs.existsSync(ZIPDIR)) {
-    fs.mkdirSync(ZIPDIR);
+if (!fs.existsSync(TMPDIR)) {
+  fs.mkdirSync(TMPDIR);
 }
 
 var masterServer = express();
@@ -24,59 +27,67 @@ masterServer.use(bodyParser.urlencoded({ extended: false }));
 masterServer.use(bodyParser.json());
 
 masterServer.get('/', (req, res) => {
+  const clientLog = "[" + req.ip + "] ";
+  console.log(clientLog + "Connected to form.");
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 masterServer.post('/analyse', (req, res) => {
-  var repoURL = req.body.repoLink;
+  const clientLog = "[" + req.ip + "] ";
+  var repoURL = req.body.repoURL;
   var tokens = repoURL.split('/');
+  var repoOwner = tokens[tokens.length - 2];
   var repoName = tokens[tokens.length - 1];
-  var clonePath = './repos/' + repoName;
+  var userHash = hashIP(req.ip).toString();
+  var clonePath = path.join(__dirname, TMPDIR, userHash);
+  console.log(clientLog + "Requested analysis of " + repoOwner + "/" + repoName);
   rmraf.sync(clonePath);
   var JSFiles = [];
 
-  //Check for connection to GitHub
-  dns.resolve('www.github.com', (err) => {
-    if (err) {
-      console.log("Unable to make a connection to GitHub.");
+  console.log(clientLog + "Cloning GitHub repository at " + repoURL + " (this may take a while)...");
+  var repo = Git.Clone(repoURL, clonePath).catch((err) => {
+    console.error(err);
+  }).then((repo) => {
+    getJSFiles(clonePath, /\.js$/);
+    console.log(clientLog + "Cloning complete! Detected " + JSFiles.length + " JS files for analysis.");
+    if (JSFiles.length <= SLAVES.length) {
+      //Simple for-loop round-robin to slaves, no need for zips
     } else {
-      console.log("GitHub connection is OK.\nCloning Git repository at " + repoURL + " (this may take a while)...");
-      var repo = Git.Clone(repoURL, clonePath).catch((err) => {
-        console.log(err);
-      }).then((repo) => {
-        getJSFiles(clonePath, /\.js$/);
-        console.log("Cloning complete!\nDetected " + JSFiles.length + " JS files for analysis.");
-        //Divide up the files to send to slaves
-        if (JSFiles.length <= SLAVES.length) {
-          //simple for-loop round-robin TODO
-        } else {
-          console.log("Dividing JS files amongst slaves...");
-          var fileChunks = splitFiles(JSFiles, SLAVES.length);
-          SLAVES.forEach((slaveIP, i) => {
-            var slaveZip = new AdmZip();
-            fileChunks[i].forEach((file) => {
-              slaveZip.addLocalFile(file);
-            });
-            const slaveZipName = repoName + "-SLAVE-" + i + ".zip"
-            const pathToSlaveZip = ZIPDIR + "/" + slaveZipName;
-            slaveZip.writeZip(pathToSlaveZip);
-            const hash = md5File.sync(pathToSlaveZip);
-            var form = new FormData();
-            form.append('JSFilesZip', fs.createReadStream(path.join(__dirname, pathToSlaveZip)));
-            form.append('checkSum', hash);
-            form.append('repoName', repoName);
-            console.log(fileChunks[i].length + " JS files will be sent to SLAVE-" + i + " at " + slaveIP + "\nMD5 Hash: " + hash);
-            form.submit('http://' + slaveIP + URL, (err, slaveRes) => {
-              if (err) {
-                console.log(err);
-              } else {
-                console.log(slaveZipName + " successfully sent to SLAVE-" + i + " for analysis.");
-              }
-            });
-          });
-          res.send("Complete?");
-        }
+      //Split files into chunks, zip them up, and send as POST multipart/form-data to slaves
+      console.log(clientLog + "Dividing JS files amongst slaves...");
+      var fileChunks = splitFiles(JSFiles, SLAVES.length);
+      SLAVES.forEach((slaveIP, i) => {
+        /* SLAVE HEALTH-CHECK
+        ping.sys.probe(slaveIP, (isAlive) => {
+          var slaveCheck = isAlive ? 'SLAVE-' + i + ' is alive.' : 'SLAVE-' + i + ' is dead.';
+          console.log(slaveCheck);
+        });
+        */
+        //Make a zip file for each slave's chunk
+        var slaveZip = new AdmZip();
+        fileChunks[i].forEach((file) => {
+          slaveZip.addLocalFile(file);
+        });
+        const slaveZipName = repoName + "-SLAVE-" + i + ".zip";
+        const pathToSlaveZip = path.join(__dirname, TMPDIR, userHash, ZIPDIR, slaveZipName);
+        slaveZip.writeZip(pathToSlaveZip);
+        //Send an MD5 checksum with the zip file to preserve integrity
+        const hash = md5File.sync(pathToSlaveZip);
+        var form = new FormData();
+        form.append('JSFilesZip', fs.createReadStream(path.join(pathToSlaveZip)));
+        form.append('checkSum', hash);
+        form.append('repoName', repoName);
+        console.log(clientLog + fileChunks[i].length + " JS files will be sent to SLAVE-" + i + " at " + slaveIP + "\nMD5 Hash: " + hash);
+        //Send the POST request to slave
+        form.submit('http://' + slaveIP + URL, (err, slaveRes) => {
+          if (err) {
+            console.error(err);
+          } else {
+            console.log(clientLog + slaveZipName + " successfully sent to SLAVE-" + i + " for analysis.");
+          }
+        });
       });
+      res.send("Complete?");
     }
   });
 
@@ -114,6 +125,13 @@ splitFiles = (files, parts) => {
     }
   }
   return result;
+}
+
+hashIP = (ip) => {
+  return ip.split("").reduce((a,b) => {
+    a = ((a<<5) - a) + b.charCodeAt(0);
+    return a & a;
+  }, 0);
 }
 
 masterServer.listen(PORT, (err) => {
--- cyclo-slave-server/slaveServer.js
+++ cyclo-slave-server/slaveServer.js
@@ -1,51 +1,63 @@
 const plato = require('plato');
 const express = require('express');
 const path = require("path");
+const fs = require('fs');
+const rmraf = require('rimraf');
 const multer = require('multer');
 const bodyParser = require("body-parser");
 const formidable = require('formidable');
 const util = require('util');
 const md5File = require("md5-file");
+const AdmZip = require('adm-zip');
+require('console-stamp')(console, { pattern: 'dd/mm/yyyy HH:MM:ss' });
 
 if (process.argv.length <= 2) {
   console.log("Usage: " + __filename + " PORT_NUMBER");
   process.exit(-1);
 }
 
+const TMPDIR = './tmp';
 const PORT = process.argv[2];
+
+if (!fs.existsSync(TMPDIR)) {
+  fs.mkdirSync(TMPDIR);
+}
 
 var slaveServer = express();
 slaveServer.use(bodyParser.urlencoded({ extended: false }));
 
-/*
-var files = [
-  './test.js',
-];
-
-var outputDir = './reports';
-var options = {
-  title: 'CS4400 Rest Service Development Task'
-};
-
-var callback = (report) => {
-  console.log("Once analysis is complete, do something.");
-};
-
-plato.inspect(files, outputDir, options, callback);
-*/
-
 slaveServer.post('/analyse', (req, res) => {
     var form = new formidable.IncomingForm();
     form.parse(req, (err, fields, files) => {
-      console.log(fields);
-      console.log(files.JSFilesZip);
       const hash = md5File.sync(files.JSFilesZip.path);
+      //Check if zip file still has integrity using the MD5 checksum supplied with the request
       if (hash === fields.checkSum) {
-        console.log("File integrity preserved.");
+        const JSFilesZip = new AdmZip(files.JSFilesZip.path);
+        const zipName = files.JSFilesZip.name;
+        const extractPath = path.join(__dirname, TMPDIR, hash)
+        JSFilesZip.extractAllTo(extractPath, true);
+        console.log(zipName + " received for analysis. Extracting to " + extractPath);
       }
       res.send("I received the ZIP file suh.")
     });
 });
+
+platoAnalysis = () => {
+  var files = [
+    './test.js',
+  ];
+
+  var outputDir = './reports';
+  var options = {
+    title: 'CS4400 Rest Service Development Task'
+  };
+
+  var callback = (report) => {
+    console.log("Once analysis is complete, do something.");
+  };
+
+  plato.inspect(files, outputDir, options, callback);
+}
 
 slaveServer.listen(PORT, (err) => {
   if (err) {
